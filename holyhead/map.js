0782a5d00f5dafd1489582b6a668f8b1d46693ed
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2023-05-08 09:08:04 +0100

    Added JS

--- holyhead/map.js
+++ holyhead/map.js
@@ -24,7 +24,9 @@ function setCircle (markers) {
 }
 
 function setCircleColours (markers) {
+    console.log(markers);
     markers.forEach(function(x) { 
+        console.log(x);
         L.circle([ x['lat'], x['lon']], 20, {color: getColour(x["cat"]), opacity:.5} ).addTo(map)
     });
 }
